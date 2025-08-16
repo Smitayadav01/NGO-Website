5bc464980817d1d10d459dba493a4e0862caf131
Smitayadav01 <960smitayadav@gmail.com>
2025-08-16 17:56:35 +0530

    server.js

--- server/server.js
+++ server/server.js
@@ -22,7 +22,7 @@ app.use(limiter);
 
 // CORS configuration
 app.use(cors({
-  origin: process.env.FRONTEND_URL || 'http://localhost:5173',
+  origin: 'https://www.hrslifecharitabletrust.com',
   credentials: true
 }));
 
